e624841961ebbbcfd8ada4f8139fd02913747da1
Aashir-Adnan <aashiradnan99@gmail.com>
2026-07-17 10:56:19 +0500

    Dump Added

--- bot/src/Database/migrations/005_guild_clocked_in_role.sql
+++ bot/src/Database/migrations/005_guild_clocked_in_role.sql
@@ -1,2 +1,6 @@
 -- Add clockedInRoleId to GuildConfig (role assigned when member clocks in; members online = clocked-in only when using this role)
-ALTER TABLE GuildConfig ADD COLUMN clockedInRoleId VARCHAR(64) NULL;
+SET @col_exists = (SELECT COUNT(*) FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'GuildConfig' AND COLUMN_NAME = 'clockedInRoleId');
+SET @sql = IF(@col_exists = 0, 'ALTER TABLE GuildConfig ADD COLUMN clockedInRoleId VARCHAR(64) NULL', 'SELECT 1');
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
